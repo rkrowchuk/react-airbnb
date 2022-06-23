50f6123600c225e1e30dbef190ec24832effd561
rkrowchuk <rhiannonkrowchuk@gmail.com>
2022-06-23 09:43:36 -0700

    update card component to render full card item with props

--- airbnb/src/Components/Card.js
+++ airbnb/src/Components/Card.js
@@ -3,24 +3,24 @@ import star from "../images/star.png";
 
 export default function Card(props) {
   let badgeText;
-  if (props.openSpots === 0) {
+  if (props.card.openSpots === 0) {
     badgeText = "SOLD OUT";
-  } else if (props.location === "Online") {
+  } else if (props.card.location === "Online") {
     badgeText = "ONLINE";
   }
   return (
     <div className="card">
-      <img src={props.img} className="card-image"/>
+      <img src={require(`../../public/images/${props.card.coverImg}`)} className="card-image"/>
       {badgeText && <h4 className='card-banner'>{badgeText}</h4>}
       <div className="card-stats">
         <img src={star} className='star-img'/>
-        <span>{props.rating}</span>
-        <span className="stat-faded">{props.reviewCount}</span>
+        <span>{props.card.stats.rating}</span>
+        <span className="stat-faded">{props.card.stats.reviewCount}</span>
         <span className="stat-faded"> • </span>
-        <span className="stat-faded"> {props.location}</span> 
+        <span className="stat-faded"> {props.card.location}</span> 
       </div>
       <p>{props.title}</p>
-      <p><b>FROM ${props.price}</b> / person</p>
+      <p><b>FROM ${props.card.price}</b> / person</p>
     </div>
   )
 }
